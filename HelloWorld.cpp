82086ac958fc087f990a8713a99b4b0f1ac2f739
Bakushin10 <shnnagai@gmail.com>
2016-09-21 18:51:47 -0400

    add helloworld

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 int main(){
 
 	std::string name;
-	std::cout << "Hello!.. who u\nMy name is: ";
+	std::cout << "Hello!.. who u\nMy name is: shin ";
 	std::cin >> name;
     std::cout << "Hello " << name << ", the Earth says, \"Hello!\"" << std::endl;
 
